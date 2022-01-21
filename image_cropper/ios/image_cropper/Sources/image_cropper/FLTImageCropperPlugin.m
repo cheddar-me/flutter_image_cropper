fbf93ed5e6e7efd7fc5539f36e810d0eb307c421
MarcinusX <marcin@fidev.io>
2022-01-21 10:45:13 +0100

    Add option to set Cancel and Done buttons colors on iOS

--- image_cropper/ios/image_cropper/Sources/image_cropper/FLTImageCropperPlugin.m
+++ image_cropper/ios/image_cropper/Sources/image_cropper/FLTImageCropperPlugin.m
@@ -146,7 +146,8 @@
     NSString *title = options[@"ios.title"];
     NSString *doneButtonTitle = options[@"ios.done_button_title"];
     NSString *cancelButtonTitle = options[@"ios.cancel_button_title"];
-
+NSNumber *doneButtonColor = options[@"ios.done_button_color"];
+    NSNumber *cancelButtonColor = options[@"ios.cancel_button_color"];
     if (minimumAspectRatio && [minimumAspectRatio isKindOfClass:[NSNumber class]]) {
         controller.minimumAspectRatio = minimumAspectRatio.floatValue;
     }
@@ -194,6 +195,12 @@
     }
     if (cancelButtonTitle && [cancelButtonTitle isKindOfClass:[NSString class]]) {
         controller.cancelButtonTitle = cancelButtonTitle;
+    }
+    if (doneButtonColor && [doneButtonColor isKindOfClass:[NSNumber class]]) {
+        controller.doneButtonColor = UIColorFromRGB([doneButtonColor intValue]);
+    }
+    if (cancelButtonColor && [cancelButtonColor isKindOfClass:[NSNumber class]]) {
+        controller.cancelButtonColor = UIColorFromRGB([cancelButtonColor intValue]);
     }
 }
 
